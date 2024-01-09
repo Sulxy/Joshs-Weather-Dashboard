c75b574c9b21342ecfe9afee9371948eeb6025e9
Sulxy <149080702+Sulxy@users.noreply.github.com>
2024-01-09 11:09:48 -0600

    Wrote the main JS functions, edited HTML to display blank placeholders on weather cards, commented out some of the JS, and made the on-click eventlistener also work when the enter key is pressed.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,64 @@
 var cityInput = document.querySelector(".city-input");
 var searchButton = document.querySelector(".search-btn");
+var currentWeatherDiv = document.querySelector(".current-weather");
+var weatherCardsDiv = document.querySelector(".weather-cards");
+
 var API_KEY = "73576cb615d23d886fc40c208c4379e5"; // OpenWeather API Key
+
+var createWeatherCard = (cityName, weatherItem, index) => {
+    var tempFahrenheit = Math.floor((weatherItem.main.temp - 273.15) * 9/5 + 32); // Convert temperature from Kelvin to Fahrenheit and remove decimal digits
+    var windSpeed = Math.floor(weatherItem.wind.speed); // Drop the integers after the decimal in the wind speed
+
+    if(index === 0) { // Big weather card
+        return `<div class="details">
+                <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                <h4>Temperature: ${tempFahrenheit}°F</h4>
+                <h4>Wind: ${windSpeed} MPH</h4>
+                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+            </div>
+            <div class="icon">
+                <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                <h4>${weatherItem.weather[0].description}</h4>  
+            </div>`;
+    } else { // Small weather cards
+        return `<li class="card"> 
+                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                <img src="http://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather icon">
+                <h4>Temp: ${tempFahrenheit}°F</h4>
+                <h4>Wind: ${windSpeed} MPH</h4>
+                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+            </li>`;
+    }
+}
 
 var getWeatherDetails = (cityName, lat, lon) => {
     var WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-        console.log(data);
-
+        // Filter the forecasts to get only one forecast per day
         var uniqueForecastDays = [];
-        data.list.filter(forecast => {
+        var fiveDayForecast = data.list.filter(forecast => {
             var forecastDate = new Date(forecast.dt_txt).getDate();
-            if(!uniqueForecastDate.includes(forecastDate)) {
+            if(!uniqueForecastDays.includes(forecastDate)) {
                 return uniqueForecastDays.push(forecastDate);
             }
         });
+
+        // Clears previous weather data
+        cityInput.value = ""; // Clear the city input
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = ""; // Clear the weather cards div
+
+        // Create weather cards for each day in the five day forecast
+        fiveDayForecast.forEach((weatherItem, index) => {
+            if(index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            }
+        });
     }).catch(() => {
-        alert("Error fetching weather forecast!");
+        alert("Error occured while fetching the weather forecast!");
     });
 }
 
@@ -35,4 +77,8 @@ var getCityCoordinates = () => {
     });
 }
 
-searchButton.addEventListener("click", getCityCoordinates);
+searchButton.addEventListener("click", getCityCoordinates);cityInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        getCityCoordinates();
+    }
+});
